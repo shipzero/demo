1cfc05d4d123b9be27e6a6920b39eba946c74d1a
Ronald Blüthl <ronald@codebeam.com>
2026-03-23 18:00:39 +0100

    fix: sigterm

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const { join } = require('node:path')
 
 const html = readFileSync(join(__dirname, 'index.html'))
 
-createServer((_, res) => {
+const server = createServer((_, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
   res.end(html)
 }).listen(3000, () => console.log('Listening on :3000'))
+
+process.on('SIGTERM', () => server.close(() => process.exit(0)))
